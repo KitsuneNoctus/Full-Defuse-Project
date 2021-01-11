b904da8b1eb0b9dede2e4059bb6dda01ad72cdad
Henry Calderon <theceintiant@gmail.com>
2021-01-11 15:02:55 -0800

    little more to AR

--- Defuse2/Defuse2/Controllers/ARSection/ARLaunchViewController.swift
+++ Defuse2/Defuse2/Controllers/ARSection/ARLaunchViewController.swift
@@ -11,6 +11,17 @@ import ARKit
 
 class ARLaunchViewController: UIViewController, ARSCNViewDelegate {
     
+    let phrases = ["You are Loved","Relax","This is You"]
+    let colors = [UIColor.black,UIColor.blue,UIColor.purple]
+    var text: SCNText!
+    
+//    let text = SCNText(string: phrases.randomElement(), extrusionDepth: 1)
+//
+//    let material = SCNMaterial()
+//    material.diffuse.contents = colors.randomElement()
+    
+//    text.materials = [material]
+    
     @IBOutlet weak var sceneView: ARSCNView!
     @IBOutlet weak var launchButton: UIButton!
     
@@ -19,7 +30,9 @@ class ARLaunchViewController: UIViewController, ARSCNViewDelegate {
         self.navigationController?.navigationBar.isHidden = false
         sceneView.delegate = self
         sceneView.showsStatistics = true
-        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin]
+//        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin]
+        
+        showItem()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -31,6 +44,22 @@ class ARLaunchViewController: UIViewController, ARSCNViewDelegate {
     
     @IBAction func launch(_ sender: UIButton) {
         print("Working")
+    }
+    
+    func showItem(){
+        self.text = SCNText(string: phrases.randomElement(), extrusionDepth: 1)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = colors.randomElement()
+        
+        text.materials = [material]
+        
+        let node = SCNNode()
+        node.position = SCNVector3(0, 0.1, -0.5)
+        node.scale = SCNVector3(0.01, 0.01, 0.01)
+        node.geometry = text
+        node.name = "shape"
+        sceneView.scene.rootNode.addChildNode(node)
     }
     
 }
